f06b8a7c6b624cb72e7531187b2b1e47770cab78
vvol0002 <vvol0002@student.monash.edu>
2024-12-18 13:30:20 +1100

    Project for access definition

---
+++ ConsoleAccess/CheckAccess.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleAccess
+{
+    public static class CheckAccess
+    {
+        private struct AccessTypes
+        {
+            public const string Free = "Free";
+            public const string Advanced = "Advanced";
+            public const string Premium = "Premium";
+        }
+
+
+        public static bool Check()
+        {
+            return true;
+        }
+    }
+}
