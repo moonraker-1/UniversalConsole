5705a3f2a6c0283022ae6f474ac9e24903645dba
vvol0002 <vvol0002@student.monash.edu>
2025-01-07 21:55:35 +1100

    Error & Information messages

--- UniversalConsoleShared/ConsoleAlert.cs
+++ UniversalConsoleShared/ConsoleAlert.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,44 +12,58 @@ namespace UniversalConsoleShared
         public static void ErrorNotFound(object item)
         {
             _showMessage($"{item?.ToString()} cannot be checked/retrieved. " +
-                $"Check if the parameters are correct.\n",
-              ConsoleColor.Red);
+                $"Check if the parameters are correct.\n");
         }
 
         public static void ErrorKeyNotFound(object key)
         {
             _showMessage($"Key {key?.ToString()} was not found. " +
-                $"Check the spelling.\n",
-              ConsoleColor.Red);
+                $"Check the spelling.\n");
         }
 
         public static void ErrorIncorrectParameter(object item)
         {
-            _showMessage($"Parameter {item?.ToString()} cannot be used. Check spelling.",
-              ConsoleColor.Red);
+            _showMessage($"Parameter {item?.ToString()} cannot be used. Check spelling.");
         }
 
         public static void ErrorUnkown()
         {
             _showMessage("Unkown error. Please, report\n" +
-                $"{Globals.contributionMessage}", ConsoleColor.Red);
+                $"{Globals.contributionMessage}");
         }
 
         public static void ErrorInternal()
         {
             _showMessage("Unkown error. Please, report\n" +
-                $"{Globals.contributionMessage}", ConsoleColor.Red);
+                $"{Globals.contributionMessage}");
         }
 
         public static void ErrorCustom(string message)
         {
-            _showMessage(message, ConsoleColor.Red);
+            _showMessage(message);
         }
 
-        private static void _showMessage(string? message, ConsoleColor color)
+        private static void _showMessage(string? message)
         {
-            Console.ForegroundColor = color;
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\n" + message + "\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    
+    public static class ConsoleInformation
+    {
+        public static void CurrentDirectoryChange(string directory)
+        {
+            _showMessage($"The directory has been changed to {directory}\n");
+        }
+
+        private static void _showMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(message);
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
