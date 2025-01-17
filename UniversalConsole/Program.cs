8defac62382a75f6eac5ffc2d16b52f2164e4486
vvol0002 <vvol0002@student.monash.edu>
2025-01-17 14:58:31 +1100

    Shorten address if longer than 45 characters

--- UniversalConsole/Program.cs
+++ UniversalConsole/Program.cs
@@ -34,13 +34,37 @@ namespace UniversalConsole
 
             while (true) 
             {
-                Console.Write(Globals.location + ">>");
+                Console.Write(shortenLocation() + ">>");
                 Input input = new Input();
                 InputData inpData = input.Take(Convert.ToString(Console.ReadLine()));
                 Command command = new Command();
                 bool lol = command.Process(inpData);
                 //Console.ReadKey();
             }
+        }
+
+        private static string shortenLocation()
+        {
+            string loc = Globals.location;
+            const string replacement = "...";
+            const int maxLength = 45;
+
+            if (loc.Length <= maxLength)
+            {
+                return loc;
+            }
+
+            int toRemove = loc.Length - maxLength;
+            int mid = loc.Length / 2;
+
+            int leftCut = mid - toRemove / 2;
+            int rightCut = mid + (toRemove + 1) / 2;
+
+            // Construct shortened string
+            string leftPart = loc.Substring(0, leftCut);
+            string rightPart = loc.Substring(rightCut);
+
+            return leftPart + replacement + rightPart;
         }
 
     }
